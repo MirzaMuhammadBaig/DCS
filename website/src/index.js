ff7e15dd9ee310cc1f7539e931bba642f03693e4
Muhammad <muhammadbaig090@gmail.com>
2023-08-07 06:14:13 +0500

    updated_from_sepolia

--- website/src/index.js
+++ website/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Sepolia } from "@thirdweb-dev/chains";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-
-const activeChain = "mumbai";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={Sepolia}>
       <Router>
         <Routes>
           <Route path="/*" element={<App />} />
